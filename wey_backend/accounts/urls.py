418438db2b1d26eb6abd938b999d29e4416c57d8
zhang <jamesarthur824248@gmail.com>
2023-10-17 23:57:24 +1030

    Sign up backends

--- wey_backend/accounts/urls.py
+++ wey_backend/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from .views import SignUpView
+
 urlpatterns = [
+    path("signup/", SignUpView.as_view(), name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="token_obtain"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- wey_backend/accounts/views.py
+++ wey_backend/accounts/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.generics import GenericAPIView
 
-# Create your views here.
+from .forms import SignupForm
+
+
+class SignUpView(GenericAPIView):
+    def post(self, request):
+        data = request.data
+        message = "sucess"
+
+        form = SignupForm(
+            {
+                "email": data.get("email"),
+                "name": data.get("name"),
+                "password1": data.get("password1"),
+                "password2": data.get("password2"),
+            }
+        )
+
+        if form.is_valid():
+            form.save()
+        else:
+            message = "error"
+
+        return JsonResponse({"status": message})
