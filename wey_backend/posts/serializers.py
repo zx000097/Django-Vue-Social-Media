67470904f70c61fef83a43e4d996c3cc97817205
zhang <jamesarthur824248@gmail.com>
2023-10-20 02:19:13 +1030

    Formatted the post creation time and serialise it to the frontend.

--- wey_backend/posts/serializers.py
+++ wey_backend/posts/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
-
 from accounts.serializers import UserSerializer
+from django.utils.timesince import timesince
 
 from .models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
     created_by = UserSerializer(read_only=True)
+    created_at = serializers.SerializerMethodField("format_created_at")
+
+    def format_created_at(self, post):
+        return timesince(post.created_at)
 
     class Meta:
         model = Post
