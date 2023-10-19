c6fa2f00b940abb24d9e26fa8c411e311549d259
zhang <jamesarthur824248@gmail.com>
2023-10-20 03:03:24 +1030

    New endpoint for creating a post.

--- wey_backend/posts/urls.py
+++ wey_backend/posts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .views import PostListView
+from .views import PostListView, PostCreateView
 
-urlpatterns = [path("all-posts/", PostListView.as_view(), name="posts")]
+urlpatterns = [
+    path("", PostListView.as_view(), name="posts"),
+    path("create", PostCreateView.as_view(), name="create"),
+]
--- wey_backend/posts/views.py
+++ wey_backend/posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import PostSerializer
 from .models import Post
@@ -11,3 +12,13 @@ class PostListView(APIView):
         posts = Post.objects.all()
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+
+
+class PostCreateView(APIView):
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            post = serializer.save(created_by=request.user)
+            post.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
