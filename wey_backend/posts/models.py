d189074eb4126e0ffe38ed02aeefbc0a1f63fddc
zhang <jamesarthur824248@gmail.com>
2023-10-19 23:27:58 +1030

    Created Post and Attachment models.

--- wey_backend/posts/models.py
+++ wey_backend/posts/models.py
@@ -1,3 +1,22 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Attachment(BaseModel):
+    image = models.ImageField(upload_to="attachments/")
+    created_by = models.ForeignKey(
+        User, related_name="post_attachments", on_delete=models.CASCADE
+    )
+
+
+class Post(BaseModel):
+    body = models.TextField(blank=True, null=True)
+    created_by = models.ForeignKey(User, related_name="posts", on_delete=models.CASCADE)
+    attachments = models.ManyToManyField(Attachment, blank=True)
