4126b08f35fbde03c92169ab424f7e5b8836224a
zhang <jamesarthur824248@gmail.com>
2023-10-21 01:18:01 +1030

    Removed the left profile panel from the feed.

--- wey_frontend/src/views/FeedView.vue
+++ wey_frontend/src/views/FeedView.vue
@@ -1,19 +1,6 @@
 <template>
   <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
-    <div class="main-left col-span-1">
-      <div class="p-4 bg-white border border-gray-200 text-center rounded-lg">
-        <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full" />
-
-        <p><strong>Code With</strong></p>
-
-        <div class="mt-6 flex space-x-8 justify-around">
-          <p class="text-xs text-gray-500">182 friends</p>
-          <p class="text-xs text-gray-500">120 posts</p>
-        </div>
-      </div>
-    </div>
-
-    <div class="main-center col-span-2 space-y-4">
+    <div class="main-center col-span-3 space-y-4">
       <div class="bg-white border border-gray-200 rounded-lg">
         <form v-on:submit.prevent="submitForm" method="post">
           <div class="p-4">
