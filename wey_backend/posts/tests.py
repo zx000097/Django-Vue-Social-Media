86551b814bb2bb67c63282b8efb961f3034c17e5
zhang <jamesarthur824248@gmail.com>
2023-10-21 02:28:00 +1030

    Some more tests for the post endpoints

--- wey_backend/posts/tests.py
+++ wey_backend/posts/tests.py
@@ -2,8 +2,32 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 
 from .models import Post
+
+
+class PostListViewTests(APITestCase):
+    def setUp(self):
+        self.user1 = get_user_model().objects.create_user(
+            name="testuser", email="testuser@gmail.com", password="test"
+        )
+        self.user2 = get_user_model().objects.create_user(
+            name="another testuser", email="anothertestuser@gmail.com", password="test"
+        )
+        Post.objects.get_or_create(body="Something", created_by=self.user1)
+        Post.objects.get_or_create(body="Something2", created_by=self.user1)
+        Post.objects.get_or_create(body="Something", created_by=self.user2)
+        user1_refresh_token = RefreshToken.for_user(self.user1)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"Bearer {user1_refresh_token.access_token}"
+        )
+
+    def test_get_all_posts(self):
+        url = reverse("posts")
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(len(response.data), 3)
 
 
 class ProfilePostListViewTests(APITestCase):
@@ -29,3 +53,34 @@ class ProfilePostListViewTests(APITestCase):
         self.assertEquals(len(response.data), 2)
         for dict in response.data:
             self.assertEquals(dict["created_by"]["id"], str(self.user1.id))
+
+
+class PostCreateViewTests(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            name="testuser", email="testuser@gmail.com", password="test"
+        )
+
+        user_refresh_token = RefreshToken.for_user(self.user)
+        self.client.credentials(
+            HTTP_AUTHORIZATION=f"Bearer {user_refresh_token.access_token}"
+        )
+
+    def test_create_post(self):
+        url = reverse("create_post")
+        response = self.client.post(url, {"created_by": self.user, "body": "Hello"})
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["body"], "Hello")
+        self.assertEqual(response.data["created_by"]["id"], str(self.user.id))
+
+    def test_create_post_fail_without_token(self):
+        self.client.credentials()
+        url = reverse("create_post")
+        response = self.client.post(url, {"created_by": self.user, "body": "Hello"})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_post_fail_with_invalid_token(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer randomtoken12345")
+        url = reverse("create_post")
+        response = self.client.post(url, {"created_by": self.user, "body": "Hello"})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
--- wey_backend/posts/urls.py
+++ wey_backend/posts/urls.py
@@ -5,5 +5,5 @@ from .views import PostListView, PostCreateView, ProfilePostListView
 urlpatterns = [
     path("", PostListView.as_view(), name="posts"),
     path("profile/", ProfilePostListView.as_view(), name="profile_posts"),
-    path("create", PostCreateView.as_view(), name="create"),
+    path("create", PostCreateView.as_view(), name="create_post"),
 ]
