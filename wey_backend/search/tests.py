71b593c9e46c5aa88ee77932f788fabe7b507e4f
zhang <jamesarthur824248@gmail.com>
2023-10-21 22:24:15 +1030

    Search endpoint also returns a list of posts that body contain the query string.

--- wey_backend/search/tests.py
+++ wey_backend/search/tests.py
@@ -3,26 +3,33 @@ from rest_framework.test import APITestCase
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
 
+from posts.models import Post
+
 
 class SearchViewTest(APITestCase):
     def setUp(self):
         self.user1 = get_user_model().objects.create(
             name="user1", email="user1@gmail.com", password="test"
         )
-        get_user_model().objects.create(
+        self.user2 = get_user_model().objects.create(
             name="user2", email="user2@gmail.com", password="test"
         )
-        get_user_model().objects.create(
+        self.abc_user = get_user_model().objects.create(
             name="abc", email="abc@gmail.com", password="test"
         )
         user1_refresh_token = RefreshToken.for_user(self.user1)
         self.client.credentials(
             HTTP_AUTHORIZATION=f"Bearer {user1_refresh_token.access_token}"
         )
+        Post.objects.create(body="i contains a user", created_by=self.abc_user)
+        Post.objects.create(body="not in query!", created_by=self.user1)
 
     def test_search_return_query_result(self):
         url = reverse("search")
         response = self.client.post(url, {"query": "user"})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 2)
-        self.assertTrue(all("user" in user["name"] for user in response.data))
+        self.assertEqual(len(response.data["users"]), 2)
+        self.assertTrue(all("user" in user["name"] for user in response.data["users"]))
+
+        self.assertEqual(len(response.data["posts"]), 1)
+        self.assertTrue("user" in response.data["posts"][0]["body"])
--- wey_backend/search/views.py
+++ wey_backend/search/views.py
@@ -4,11 +4,23 @@ from rest_framework import status
 
 from accounts.models import User
 from accounts.serializers import UserSerializer
+from posts.models import Post
+from posts.serializers import PostSerializer
 
 
 class SearchView(APIView):
     def post(self, request):
-        users = User.objects.filter(name__icontains=request.data["query"])
+        query = request.data["query"]
+        users = User.objects.filter(name__icontains=query)
         users_seralizer = UserSerializer(users, many=True)
 
-        return Response(users_seralizer.data, status=status.HTTP_200_OK)
+        posts = Post.objects.filter(body__icontains=query)
+        posts_serializer = PostSerializer(posts, many=True)
+
+        return Response(
+            {
+                "users": users_seralizer.data,
+                "posts": posts_serializer.data,
+            },
+            status=status.HTTP_200_OK,
+        )
