179e05bc67a0c3153ac157180608313c84948ed6
zhang <jamesarthur824248@gmail.com>
2023-10-21 22:25:12 +1030

    removed some dummy data and display the user cards based on the users from the backend response.

--- wey_frontend/src/views/SearchView.vue
+++ wey_frontend/src/views/SearchView.vue
@@ -2,65 +2,30 @@
   <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
     <div class="main-left col-span-3 space-y-4">
       <div class="bg-white border border-gray-200 rounded-lg">
-        <div class="p-4 flex space-x-4">
+        <form v-on:submit.prevent="submitForm" class="p-4 flex space-x-4">
           <input
+            v-model="query"
             type="search"
             class="p-4 w-full bg-gray-100 rounded-lg"
             placeholder="What are you looking for"
           />
-          <a href="#" class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">Post</a>
-        </div>
+          <button href="#" class="inline-block py-4 px-6 bg-purple-600 text-white rounded-lg">
+            Post
+          </button>
+        </form>
       </div>
 
       <div class="p-4 bg-white border border-gray-200 rounded-lg grid grid-cols-4 gap-4">
-        <div class="p-4 text-center bg-gray-100 rounded-lg">
+        <div
+          class="p-4 text-center bg-gray-100 rounded-lg"
+          v-for="user in users"
+          v-bind:key="user.id"
+        >
           <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full" />
 
-          <p><strong>Code With Stein</strong></p>
-
-          <div class="mt-6 flex space-x-8 justify-around">
-            <p class="text-xs text-gray-500">182 friends</p>
-            <p class="text-xs text-gray-500">120 posts</p>
-          </div>
-        </div>
-
-        <div class="p-4 text-center bg-gray-100 rounded-lg">
-          <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full" />
-
-          <p><strong>Code With Stein</strong></p>
-
-          <div class="mt-6 flex space-x-8 justify-around">
-            <p class="text-xs text-gray-500">182 friends</p>
-            <p class="text-xs text-gray-500">120 posts</p>
-          </div>
-        </div>
-
-        <div class="p-4 text-center bg-gray-100 rounded-lg">
-          <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full" />
-
-          <p><strong>Code With Stein</strong></p>
-
-          <div class="mt-6 flex space-x-8 justify-around">
-            <p class="text-xs text-gray-500">182 friends</p>
-            <p class="text-xs text-gray-500">120 posts</p>
-          </div>
-        </div>
-
-        <div class="p-4 text-center bg-gray-100 rounded-lg">
-          <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full" />
-
-          <p><strong>Code With Stein</strong></p>
-
-          <div class="mt-6 flex space-x-8 justify-around">
-            <p class="text-xs text-gray-500">182 friends</p>
-            <p class="text-xs text-gray-500">120 posts</p>
-          </div>
-        </div>
-
-        <div class="p-4 text-center bg-gray-100 rounded-lg">
-          <img src="https://i.pravatar.cc/300?img=70" class="mb-6 rounded-full" />
-
-          <p><strong>Code With Stein</strong></p>
+          <p>
+            <strong>{{ user.name }}</strong>
+          </p>
 
           <div class="mt-6 flex space-x-8 justify-around">
             <p class="text-xs text-gray-500">182 friends</p>
@@ -155,6 +120,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
 import Trends from '../components/Trends.vue'
 
@@ -163,6 +129,28 @@ export default {
   components: {
     PeopleYouMayKnow,
     Trends
+  },
+  data() {
+    return {
+      query: '',
+      users: []
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log(this.query)
+
+      axios
+        .post('search/', {
+          query: this.query
+        })
+        .then((response) => {
+          this.users = response.data
+        })
+        .catch((error) => {
+          console.log('error', error)
+        })
+    }
   }
 }
 </script>
